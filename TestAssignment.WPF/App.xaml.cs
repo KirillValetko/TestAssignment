58885eb76fe68cb6a3979c333110f4e0dcd064e4
KirillValetko <kirya_valet@mail.ru>
2024-02-01 10:32:35 +0300

    Add dependency injection

--- TestAssignment.WPF/App.xaml.cs
+++ TestAssignment.WPF/App.xaml.cs
@@ -1,8 +1,35 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.Hosting;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace TestAssignment.WPF
 {
     public partial class App : Application
     {
+        public static IHost AppHost { get; private set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost.StartAsync();
+            var startUpForm = AppHost.Services.GetRequiredService<MainWindow>();
+            startUpForm.Show();
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost.StopAsync();
+            base.OnExit(e);
+        }
     }
 }
