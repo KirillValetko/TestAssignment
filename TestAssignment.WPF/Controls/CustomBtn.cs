bb99a3c2ec8e15e65687ea3a6cf19ed3974438b5
KirillValetko <kirya_valet@mail.ru>
2024-02-01 20:05:48 +0300

    Add custom button and relay command

---
+++ TestAssignment.WPF/Controls/CustomBtn.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TestAssignment.WPF.Controls
+{
+    public class CustomBtn : RadioButton
+    {
+        static CustomBtn()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomBtn), 
+                new FrameworkPropertyMetadata(typeof(CustomBtn)));
+        }
+    }
+}
