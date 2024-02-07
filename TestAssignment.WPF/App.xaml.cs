ebc93a29ed62f29f3cb88f2c4edd810c87f1432b
KirillValetko <kirya_valet@mail.ru>
2024-02-07 09:22:49 +0300

    Add configuration

--- TestAssignment.WPF/App.xaml.cs
+++ TestAssignment.WPF/App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using System.IO;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TestAssignment.WPF.Infrastructure.Configuration;
 
@@ -8,15 +10,26 @@ namespace TestAssignment.WPF
     public partial class App : Application
     {
         public static IHost AppHost { get; private set; }
+        public static IConfiguration Configuration { get; private set; }
 
         public App()
         {
             AppHost = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((_, configuration) =>
+                {
+                    configuration
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddUserSecrets(GetType().Assembly, optional: false, reloadOnChange: true);
+                    Configuration = configuration.Build();
+                })
                 .ConfigureServices((_, services) =>
                 {
                     services.InitMainWindow();
                     services.InitViewModels();
                     services.InitGenerators();
+                    services.InitSettings(Configuration);
+                    services.InitDbContext();
+                    services.InitRepositories();
                     services.InitServices();
                     services.InitNavigation();
                 })
